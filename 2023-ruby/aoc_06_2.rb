3ecadf664c7b572ab62f8a70f09bb56ac31413cb
Shellburn <shellburnnet@gmail.com>
2023-12-28 15:23:29 +0200

    up

---
+++ 2023-ruby/aoc_06_2.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/env ruby
+
+# @filename = "data_06_1_sample.txt"
+@filename = "data_06_1.txt"
+
+Race = Struct.new(:id, :time, :distance)
+
+def read_data
+  array = []
+  time_line = []
+  dist_line = []
+  File.open(@filename) do |file|
+    time_line = [file.gets.strip.split(' ').drop(1).join.to_i]
+    dist_line = [file.gets.strip.split(' ').drop(1).join.to_i]
+  end
+
+  time_line.each_with_index do |i, idx|
+    array << Race.new(idx, time_line[idx], dist_line[idx])
+  end
+
+  return array
+end
+
+def get_distances(race)
+  distances = []
+  (race.time + 1).times do |hold_time|
+    race_time = race.time - hold_time
+    distance = hold_time * race_time
+    # pp "hold/speed: #{hold_time}, race_time: #{race_time}, distance: #{distance}"
+    distances << distance
+  end
+  return distances
+end
+
+def win_count(race)
+  record = race.distance
+  wins = 0
+  get_distances(race).each do |i|
+    if record < i
+      wins += 1
+    end
+  end
+  # pp wins
+  return wins
+end
+
+def get_wins(races)
+  wins = []
+  races.each do |race|
+    wins << win_count(race)
+  end
+  return wins
+end
+
+pp races = read_data
+# pp get_distances(races[0])
+# pp win_count(races[0])
+
+pp get_wins(races).inject {|result, element| result * element}
+
---
+++ 2023-ruby/aoc_07_1.rb
@@ -0,0 +1,110 @@
+#!/usr/bin/env ruby
+
+# @filename = "data_07_1_sample.txt"
+# @filename = "data_07_2_sample.txt"
+@filename = "data_07_1.txt"
+
+# power rules:
+# five of kind  - 60'000'000
+# four of kind  - 50'000'000
+# full house    - 40'000'000
+# three of kind - 30'000'000
+# two pairs     - 20'000'000
+# one pair      - 10'000'000
+# high card     - 0
+# 2 .. 9, T(10), J(12), Q(13), K(14), A(15)
+# 1st - 10'000x, 2nd - 1'000x, 3rd - 100x, 4th - 10x, 5th - 1x
+
+Game = Struct.new(:hand, :bid, :power)
+
+def read_data
+  array = []
+  File.foreach(@filename) do |line|
+    line.strip!
+    hand, bid = line.split(' ')
+    # pp "hand: #{hand}, bid: #{bid}"
+    array << Game.new(hand.chars, bid.to_i, nil)
+  end
+  return array
+end
+
+def get_card_value(card)
+  hash = {
+    '2' => 2,
+    '3' => 3,
+    '4' => 4,
+    '5' => 5,
+    '6' => 6,
+    '7' => 7,
+    '8' => 8,
+    '9' => 9,
+    'T' => 10,
+    'J' => 12,
+    'Q' => 13,
+    'K' => 14,
+    'A' => 15,
+  }
+
+  hash[card]
+end
+
+def get_hand_power(hand)
+  power = 0
+  counts = Hash.new(0)
+  hand.each { |card| counts[card] += 1 }
+  counts_sorted = counts.sort_by { |key, value| value }.reverse
+  pp counts_sorted
+  if counts_sorted[0][1] == 5
+    pp "#{hand.join}: five of kind"
+    power += 60000000000
+  elsif counts_sorted[0][1] == 4
+    power += 50000000000
+    pp "#{hand.join}: four of kind"
+  elsif counts_sorted[0][1] == 3 and counts_sorted[1][1] == 2
+    power += 40000000000
+    pp "#{hand.join}: full of house"
+  elsif counts_sorted[0][1] == 3 and counts_sorted[1][1] == 1
+    power += 30000000000
+    pp "#{hand.join}: three of kind"
+  elsif counts_sorted[0][1] == 2 and counts_sorted[1][1] == 2
+    power += 20000000000
+    pp "#{hand.join}: two pairs"
+  elsif counts_sorted[0][1] == 2 and counts_sorted[1][1] == 1
+    power += 10000000000
+    pp "#{hand.join}: one pair"
+  elsif counts_sorted[0][1] == 1
+    power += 0 #
+    pp "#{hand.join}: high card"
+  else
+    raise "Unknown combination [#{hand.join}]"
+  end
+
+  pp hand
+  pp "#{100000000 * get_card_value(hand[0])} #{1000000 * get_card_value(hand[1])} #{10000 * get_card_value(hand[2])} #{100 * get_card_value(hand[3])} #{get_card_value(hand[4])}"
+  pp power += 100000000 * get_card_value(hand[0])
+  pp power += 1000000   * get_card_value(hand[1])
+  pp power += 10000     * get_card_value(hand[2])
+  pp power += 100       * get_card_value(hand[3])
+  pp power += 1         * get_card_value(hand[4])
+
+  pp power
+  return power
+end
+
+def set_games_power(games)
+  games.each { |game| game.power = get_hand_power(game.hand) }
+end
+
+games = read_data
+set_games_power(games)
+# pp games
+
+games_sorted = games.sort_by { |game| game.power }
+
+pp games_sorted
+
+sum = 0
+games_sorted.each_with_index do |game, idx|
+  sum += game.bid * (idx + 1)
+end
+pp sum
